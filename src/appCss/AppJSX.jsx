c5f91121da9c6ae3d8cb09e778a1d185733199d1
장인천 <icjang@realsn.com>
2023-02-20 14:50:32 +0900

    추가

---
+++ src/appCss/AppJSX.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled, {css} from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+`
+
+const Button = styled.button`
+  border-radius : 3px;
+  ${(props) => 
+    props.primary && css`
+      background: #009cd5;
+      color: white;
+    `
+  }
+  
+`
+
+export default function App() {
+
+  return (
+    <>
+      <Container>
+        <Button>nomal</Button>
+        <Button primary>primary</Button>
+      </Container>
+      <div>
+        <h1 className="text-3xl">안녕</h1>
+      </div>
+    </>
+  );
+}
+
